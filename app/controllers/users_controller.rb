c3cb4b57302580ec84cea1d535a5683896db30e8
Kenny <chenkenn@gmail.com>
2015-04-28 08:40:27 -0600

    user create complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
   def show
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:message] = "User has been successfully created!"
+      redirect_to user_path(@user)
+    else
+      flash[:errors] = @user.errors.full_messages(', ').join
+      render :new
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   resource :user
-
   get 'login', to: 'sessions#new'
 
   post 'login', to: 'sessions#create'
