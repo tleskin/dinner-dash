a64666966d8473cdf54669e680bfab6db90103a4
Carmer <carmerandrew@gmail.com>
2015-04-28 14:28:35 -0600

    fixed hound violations

--- spec/features/admin_can_view_items_list_spec.rb
+++ spec/features/admin_can_view_items_list_spec.rb
@@ -1,15 +1,13 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "admin items" do
   context "when logged in as admin" do
     it "can view list of items" do
-      created_items = create_list(:item, 5)
+      create_list(:item, 5)
       admin = create(:admin_user)
-      # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-      visit login_path
-      fill_in "Username", with: "JaneDoe"
-      fill_in "Password", with: "password"
-      click_button "Submit"
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit admin_dashboard_path
 
       click_link "All Items"
 
