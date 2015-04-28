be5350c4ef0fa2de6b8187124028c2a38871bf63
Thomas Leskin <tleskin@gmail.com>
2015-04-28 15:07:42 -0600

    Admin can add a new item with feature test

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,5 +1,30 @@
 class Admin::ItemsController < Admin::BaseController
+  def new
+    @item = Item.new
+  end
+
   def index
     @items = Item.all
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:notice] = "#{@item.title} created!"
+      redirect_to admin_item_path(@item)
+    else
+      flash[:error] = @item.errors.full_messages.join(", ")
+      render :new
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :price)
   end
 end
