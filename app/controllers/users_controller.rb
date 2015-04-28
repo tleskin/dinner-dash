7f71b0225d382e731311c6a6d60401efe019044f
Kenny <chenkenn@gmail.com>
2015-04-28 10:41:49 -0600

    fixed hound notifiications

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,15 +14,20 @@ class UsersController < ApplicationController
       flash[:message] = "User has been successfully created!"
       redirect_to user_path(@user)
     else
-      flash[:errors] = @user.errors.full_messages(', ').join
+      flash[:errors] = @user.errors.full_messages(", ").join
       render :new
     end
   end
 
-
   private
 
   def user_params
-    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+    params.require(:user).permit(
+                                  :name,
+                                  :username,
+                                  :email,
+                                  :password,
+                                  :password_confirmation
+                                  )
   end
 end
--- spec/features/user_view_spec.rb
+++ spec/features/user_view_spec.rb
@@ -1,6 +1,6 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe '' do
+RSpec.describe "User view" do
   context 'with valid attributes' do
     it 'can create a new user' do
       visit new_user_path
@@ -15,19 +15,4 @@ RSpec.describe '' do
       expect(page).to have_content("davy")
     end
   end
-
-  # context 'with invalid addtributes' do
-  #   it 'cannot create a new user' do
-  #     visit new_user_path
-
-  #     fill_in "Name", with: "David"
-  #     fill_in "Username", with: "davy"
-  #     fill_in "Email", with: "davidgmail.com"
-  #     fill_in "Password", with: "password"
-  #     fill_in "Password confirmation", with: "password"
-  #     click_link_or_button "Create User"
-
-  #     expect(current_path).to eq(new_user_path)
-  #   end
-  # end
 end
