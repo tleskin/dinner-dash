7e39eb94072edfba631770109d3a7b92046e6deb
Carmer <carmerandrew@gmail.com>
2015-05-04 13:47:10 -0600

    added validation for item title uniqueness

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ActiveRecord::Base
                                    }, default_url: "fork_knife_thumb.jpg"
 
   validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg", "image/png"]
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :description, presence: true
   validates :price, presence: true
   validate  :has_categories?
