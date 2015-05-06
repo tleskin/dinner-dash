c1595b338230bd1a92ca5d59334bb2dcc9b3423d
Thomas Leskin <tleskin@gmail.com>
2015-05-05 23:08:44 -0600

    Integrated mailer with SendGrid, working for user account creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
+      UserNotifier.send_signup_email(@user).deliver_now
       flash[:message] = "User has been successfully created!"
       redirect_to user_path
     else
---
+++ app/mailers/user_notifier.rb
@@ -0,0 +1,9 @@
+class UserNotifier < ApplicationMailer
+  default :from => 'hipsterbites@gmail.com'
+
+  def send_signup_email(user)
+    @user = user
+    mail( :to => @user.email,
+    :subject => 'Thanks for signing up for with Hipster Bytes' )
+  end
+end
