578cbb2b507bdfd0d36da70d02939f77bacc7bf1
Thomas Leskin <tleskin@gmail.com>
2015-05-05 23:18:29 -0600

    Fixed Hound violations

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,9 +1,9 @@
 class UserNotifier < ApplicationMailer
-  default :from => 'hipsterbites@gmail.com'
+  default from: "hipsterbites@gmail.com"
 
   def send_signup_email(user)
     @user = user
-    mail( :to => @user.email,
-    :subject => 'Thanks for signing up for with Hipster Bytes' )
+    mail(to: @user.email,
+         subject: "Thanks for signing up for with Hipster Bytes")
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -21,13 +21,13 @@ module HipsterBytes
     config.action_mailer.delivery_method = :smtp
 
     ActionMailer::Base.smtp_settings = {
-      :user_name => ENV["SENDGRID_USERNAME"],
-      :password => ENV["SENDGRID_PASSWORD"],
-      :domain => 'hipsterbytes.herokuapp.com',
-      :address => 'smtp.sendgrid.net',
-      :port => 587,
-      :authentication => :plain,
-      :enable_starttls_auto => true
+      user_name: ENV["SENDGRID_USERNAME"],
+      password: ENV["SENDGRID_PASSWORD"],
+      domain: "hipsterbytes.herokuapp.com",
+      address: "smtp.sendgrid.net",
+      port: 587,
+      authentication: :plain,
+      enable_starttls_auto: true
     }
   end
 end
