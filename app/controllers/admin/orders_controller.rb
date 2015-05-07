28bffefb1a81dfc7771b9e1ad8950047d98b67db
Kenny <chenkenn@gmail.com>
2015-05-06 18:34:08 -0600

    tests fixed

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,7 @@
 class Admin::OrdersController < Admin::BaseController
   def dashboard
     @orders = Order.all
+    @statuses = ["ordered", "completed", "cancelled", "paid"]
     render :dashboard
   end
 
